0b287de66e6e71d74b3b3efae102f767dd59bb8f
Jorge Giner <jorge.giner@hotmail.com>
2023-06-19 15:34:48 +0200

    * New EXPORT test.

--- tests/export.asm
+++ tests/export.asm
@@ -1,9 +1,13 @@
-	.EXPORT label1, label2, label6789012345678
+	.EXPORT 
+	.EXPORT label1,
+	.EXPORT label3
+	.EXPORT label2, label6789012345678
 
 	.ORG $4000
 
 label1	LD A,B
 label2	LD B,C
+label3  LD A,B
 label6789012345678 LD B,C
 
 	.END
